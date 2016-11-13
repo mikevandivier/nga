77ee91f3e9e5fd8db3a6044a6498a1e8f3192ff8
Vandivier <vandivier_john@yahoo.com>
2016-11-13 00:50:21 -0500

    add basic todos;

--- app/home/home.js
+++ app/home/home.js
@@ -6,9 +6,25 @@ angular.module('nga.home', ['ngRoute'])
   $routeProvider.when('/home', {
     templateUrl: 'home/home.html',
     controller: 'HomeCtrl'
-  });
+  })
 }])
 
-.controller('HomeCtrl', [function() {
+.controller('HomeCtrl', ['$scope', function($scope) {                   //ref: http://stackoverflow.com/questions/16284769/angularjs-scope-undefined-when-controllers-are-inside-a-module
+  
+    $scope.todoList = [{todoText:'Clean House', done:false}]
 
-}]);
+    $scope.todoAdd = function() {                                       //ref: http://www.w3schools.com/angular/tryit.asp?filename=try_ng_todo_app
+        $scope.todoList.push({todoText:$scope.todoInput, done:false})
+        $scope.todoInput = ""
+    };
+
+    $scope.remove = function() {
+        var oldList = $scope.todoList;
+
+        $scope.todoList = []
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.todoList.push(x)
+        })
+    }
+    
+}])
